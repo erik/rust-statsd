e5afb6ffede03aac41df5eb53232e16ea1424c39
Erik Price <erik.price16@gmail.com>
2013-11-25 22:01:26 -0800

    Clean up main.rs a bit.
    For some nebulous definition of "clean up."

--- src/statsd/server/main.rs
+++ src/statsd/server/main.rs
@@ -14,69 +14,92 @@ use std::str;
 
 use extra::arc::MutexArc;
 
-
 static FLUSH_INTERVAL_MS: u64 = 10000;
-static MAX_PACKET_SIZE: uint = 1024;
+static MAX_PACKET_SIZE: uint = 256;
+
+
+enum Event {
+    FlushTimer,
+    UdpMessage(~[u8]),
+    TcpMessage(~tcp::TcpStream)
+}
+
+
+/// Run in a new task for each management connection made to the server.
+fn management_connection_loop(stream: ~tcp::TcpStream,
+                              buckets_arc: MutexArc<Buckets>) {
+    let mut stream = buffered::BufferedStream::new(*stream);
+
+    loop {
+        // XXX: this will fail if non-utf8 characters are used
+        match stream.read_line() {
+            Some(line) => do buckets_arc.access |buckets| {
+                let resp = buckets.handle_management_cmd(line);
+
+                stream.write(resp.as_bytes());
+                stream.flush();
+            },
+            None => { break; }
+        }
+    }
+}
+
+
+fn flush_timer_loop(chan: SharedChan<~Event>) {
+    let mut timer = Timer::new().unwrap();
+    let periodic = timer.periodic(FLUSH_INTERVAL_MS);
+
+    loop {
+        periodic.recv();
+        chan.send(~FlushTimer);
+    }
+}
+
+
+fn management_server_loop(chan: SharedChan<~Event>) {
+    let addr: SocketAddr = FromStr::from_str("0.0.0.0:8126").unwrap();
+    let listener = tcp::TcpListener::bind(addr).unwrap();
+    let mut acceptor = listener.listen();
+
+    for stream in acceptor.incoming() {
+        stream.map(|stream| {
+            chan.send(~TcpMessage(~stream));
+        });
+    }
+}
+
+fn udp_server_loop(chan: SharedChan<~Event>) {
+    let addr: SocketAddr = FromStr::from_str("0.0.0.0:8125").unwrap();
+    let mut socket = UdpSocket::bind(addr).unwrap();
+    let mut buf = [0u8, ..MAX_PACKET_SIZE];
+
+    loop {
+        socket.recvfrom(buf).map(|(nread, _)| {
+            // Messages this large probably are bad in some way.
+            if nread == MAX_PACKET_SIZE {
+                warn!("Max packet size exceeded.");
+            }
+
+            // Use the slice to strip out trailing \0 characters
+            let msg = buf.slice_to(nread).to_owned();
+            chan.send(~UdpMessage(msg));
+        });
+    }
+}
 
 
 #[main]
 fn main() {
-
-    enum Event {
-        FlushTimer,
-        UdpMessage(~[u8]),
-        TcpMessage(~tcp::TcpStream)
-    }
-
     let (event_port, event_chan_): (Port<~Event>, Chan<~Event>) = stream();
     let event_chan = SharedChan::new(event_chan_);
 
-    // UDP server loop
-    let udp_chan = event_chan.clone();
-    do spawn {
-        let addr: SocketAddr = FromStr::from_str("0.0.0.0:9991").unwrap();
-        let mut socket = UdpSocket::bind(addr).unwrap();
-        let mut buf = [0u8, ..MAX_PACKET_SIZE];
-
-        loop {
-            socket.recvfrom(buf).map(|(nread, _)| {
-                // Messages this large probably are bad in some way.
-                if nread == MAX_PACKET_SIZE {
-                    warn!("Max packet size exceeded.");
-                }
-
-                // Use the slice to strip out trailing \0 characters
-                let msg = buf.slice_to(nread).to_owned();
-                udp_chan.send(~UdpMessage(msg));
-            });
-        }
-    }
-
-    // Flush timer loop
     let flush_chan = event_chan.clone();
-    do spawn {
-        let mut timer = Timer::new().unwrap();
-        let periodic = timer.periodic(FLUSH_INTERVAL_MS);
-
-        loop {
-            periodic.recv();
-            flush_chan.send(~FlushTimer);
-        }
-    }
-
-    // Management server loop
     let mgmt_chan = event_chan.clone();
-    do spawn {
-        let addr: SocketAddr = FromStr::from_str("0.0.0.0:8126").unwrap();
-        let listener = tcp::TcpListener::bind(addr).unwrap();
-        let mut acceptor = listener.listen();
+    let udp_chan = event_chan.clone();
 
-        for stream in acceptor.incoming() {
-            stream.map(|stream| {
-                mgmt_chan.send(~TcpMessage(~stream));
-            });
-        }
-    }
+    spawn(|| flush_timer_loop(flush_chan));
+    spawn(|| management_server_loop(mgmt_chan));
+    spawn(|| udp_server_loop(udp_chan));
 
     let buckets = Buckets::new();
     let buckets_arc = MutexArc::new(buckets);
@@ -85,40 +108,24 @@ fn main() {
     loop {
         match *event_port.recv() {
             // Flush timeout
-            FlushTimer => do buckets_arc.access |buckets| {
-                buckets.flush();
-            },
+            FlushTimer => buckets_arc.access(|buckets| buckets.flush()),
 
             // Management server
             TcpMessage(s) => {
                 // Clone the arc so the new task gets its own copy.
                 let buckets_arc = buckets_arc.clone();
 
-                do spawn {
-                    let mut stream = buffered::BufferedStream::new(*s);
-
-                    loop {
-                        // XXX: this will fail if non-utf8 characters are used
-                        match stream.read_line() {
-                            Some(line) => do buckets_arc.access |buckets| {
-                                let resp = buckets.handle_management_cmd(line);
-
-                                stream.write(resp.as_bytes());
-                                stream.flush();
-                            },
-                            None => { break; }
-                        }
-                    }
-                }
+                // Spin up a new thread to handle the TCP stream.
+                spawn(|| management_connection_loop(s, buckets_arc));
             },
 
             // UDP message received
-            UdpMessage(buf) => do buckets_arc.access |buckets| {
+            UdpMessage(buf) => buckets_arc.access(|buckets| {
                 str::from_utf8_opt(buf)
                     .and_then(|string| FromStr::from_str(string))
                     .map(|metric| buckets.add_metric(metric))
                     .or_else(|| { buckets.bad_messages += 1; None });
-            },
+            }),
         }
     }
 }
